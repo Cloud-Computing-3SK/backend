36946bc2fbc46b73b1e4ae3eadb16c3a27a46ce0
syifakaffa <syifa.kaffa@ui.ac.id>
2025-12-12 12:02:31 +0700

    Add STATIC_ROOT and MEDIA settings to improve file handling

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # ADD THIS LINE
+
+# Media files
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'  # ADD THIS TOO
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
